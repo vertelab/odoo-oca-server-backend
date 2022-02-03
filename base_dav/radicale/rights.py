c3b0c0b067157b63b3aed14bc0ef42a7c506dc62
iammiracle <miracleaayodele@gmail.com>
2022-02-03 18:07:49 +0100

    base dav migration to 14.0

---
+++ base_dav/radicale/rights.py
@@ -0,0 +1,36 @@
+# Copyright 2018 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from .collection import Collection
+
+try:
+    from radicale.rights import (
+        AuthenticatedRights, OwnerWriteRights, OwnerOnlyRights,
+    )
+except ImportError:
+    AuthenticatedRights = OwnerOnlyRights = OwnerWriteRights = None
+
+
+class Rights:
+    def __init__(self, OwnerOnlyRights, OwnerWriteRights, AuthenticatedRights):
+        self.OwnerOnlyRights = OwnerOnlyRights
+        self.OwnerWriteRights = OwnerWriteRights
+        self.AuthenticatedRights = AuthenticatedRights
+
+    def authorized(self, user, path, perm):
+        if path == '/':
+            return True
+
+        collection = Collection(path)
+        if not collection.collection:
+            return False
+
+        rights = collection.collection.sudo().rights
+        cls = {
+            "owner_only": self.OwnerOnlyRights,
+            "owner_write_only": self.OwnerWriteRights,
+            "authenticated": self.AuthenticatedRights,
+        }.get(rights)
+        if not cls:
+            return False
+        return cls.authorized(self, user, path, perm)
